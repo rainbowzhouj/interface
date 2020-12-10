7418f857269ff29570b84b11b302fc06f84abdfa
rainbowzhouj <1004385861@qq.com>
2020-12-10 11:25:44 +0800

    - 封装公共接口，实现代码分层

--- tag.py
+++ tag.py
@@ -2,71 +2,120 @@ import json
 
 import requests
 
+from base_api import BaseApi
 
-class Tag:
+
+class Tag(BaseApi):
     def __init__(self):
-        self.token = self.get_token()
+        super().__init__()
 
-    def get_token(self):
-        ID = 'ww342c4e9a1d9b5638'
-        SECRET = 'uVivGfU7O7MrbumFBC77kfkqEw_FS8j5qADaAYOFBGs'
-        r = requests.get('https://qyapi.weixin.qq.com/cgi-bin/gettoken',
-                         params={"corpid": ID,
-                                 "corpsecret": SECRET})
-        print(json.dumps(r.json(), indent=2))
-        token = r.json()['access_token']
-        return token
+    def add(self, group_name, tag_name, **kwargs):
+        data = {
+            "method": "post",
+            "url": "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag",
+            "params": {"access_token": self.token},
+            "json": {"group_name": group_name,
+                     "tag": tag_name,
+                     **kwargs
+                     }}
+        return self.send(data)
 
-    def add(self):
-        r = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag',
-            params={"access_token": self.token},
-            json={
-                "group_name": 'app',
-                "order": 6,
-                "tag": [
-                    {
-                        "name": 'Appnium',
-                        "order": 1
-                    }
-                ]
-            }
-        )
-        print(json.dumps(r.json(), indent=2))
-        return r
+        # step 1  => step1 false
+        # step 2  => step2 true
+        # step 3  => step3 true
+
+    def add_and_detect(self, group_name, tag, **kwargs):
+        r = self.add(group_name, tag, **kwargs)
+        # 如果删除的元素已经存在
+        if r.json()["errcode"] == 40071:
+            group_id = self.find_group_id_by_name(group_name)
+            if not group_id:
+                # 元素不在，接口有问题
+                return ""
+            self.delete_group([group_id])
+            self.add(group_name, tag, **kwargs)
+        result = self.find_group_id_by_name(group_name)
+        if not result:
+            print("add not success")
+        return result
 
     def list(self):
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list'
-        r = requests.post(url=url, params={"access_token": self.token}, json={'tag_id': []})
-        print(json.dumps(r.json(), indent=2))
-        return r
+        data = {
+            "method": "get",
+            "url": 'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list',
+            "params": {"access_token": self.token},
+            "json": {'tag_id': []}}
+        return self.send(data)
 
     def update(self, id, tag_name):
-        r = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/edit_corp_tag',
-            params={"access_token": self.token},
-            json={
-                'id': id,
-                'name': tag_name
-            }
-        )
-        print(json.dumps(r.json(), indent=2))
-        return r
+        data = {
+            "method": "post",
+            "url": 'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/edit_corp_tag',
+            "params": {"access_token": self.token},
+            "json": {"id": id,
+                     "name": tag_name
+                     }}
+        return self.send(data)
 
-    def delete(self):
-        r = requests.post(
-            'hhttps://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag',
-            params={"access_token": self.token},
-            json={
-                "group_name": 'app',
-                "order": 6,
-                "tag": [
-                    {
-                        "name": 'Appnium',
-                        "order": 1
-                    }
-                ]
-            }
-        )
-        print(json.dumps(r.json(), indent=2))
+    # 查询 tag_id ->  删除 tag_id
+    # 如果正常： 成功
+    # {
+    #     "errcode": 0,
+    #     "errmsg": "ok"
+    # }
+    # 如果异常： 失败（）
+    # 　手动获取
+    def delete_group(self, group_id):
+        data = {
+            "method": "post",
+            "url": 'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag',
+            "params": {"access_token": self.token},
+            "json": {
+                "group_id": group_id
+                # "tag_id": ["etXTmQCgAArgoBlBP8B052rVqpbJ-d5g"],
+            }}
+        return self.send(data)
+
+    def delete_tag(self, tag_id):
+        data = {
+            "method": "post",
+            "url": 'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag',
+            "params": {"access_token": self.token},
+            "json": {
+                # "group_id": group_id
+                "tag_id": tag_id
+            }}
+        return self.send(data)
+
+    def is_group_id_exist(self, group_id):
+        # 查询元素id是否存在，如果不存在，报错
+        for group in self.list().json()["tag_group"]:
+            if group_id in group["group_id"]:
+                return True
+        print("group id not in group")
+        return False
+
+    def find_group_id_by_name(self, group_name):
+        # 查询元素名称是否存在，如果不存在，报错
+        for group in self.list().json()["tag_group"]:
+            if group_name in group["group_name"]:
+                return group["group_id"]
+        print("group name not in group")
+        # 注意不同原因导致的同一错误现象，需区分
+        return ""
+
+    def delete_and_detect_group(self, group_ids):
+        deleted_group_ids = []
+        r = self.delete_group(group_ids)
+        if r.json()["errcode"] == 40068:
+            # 如果标签不存在，就添加一个标签，将它的 group_id存储进来
+            for group_id in group_ids:
+                if not self.is_group_id_exist(group_id):
+                    group_id_tmp = self.add_and_detect(group_name="UI",
+                                                       tag=[{"name": "web"}])
+                    deleted_group_ids.append(group_id_tmp)
+                    # 如果标签存在，就将它存入标签组
+                else:
+                    deleted_group_ids.append(group_id)
+        r = self.delete_group(deleted_group_ids)
         return r
--- test_tag.py
+++ test_tag.py
@@ -16,13 +16,12 @@ class TestTag:
     # ])
 
     def test_tag_add(self):
-        r = self.tag.add(
-            # group=group,
-            # tags=tags   ,group,tags
-        )
-
-        r = self.tag.list()
-        assert jsonpath(r.json(), f"$..[?(@.name=='app')]")[0]['name'] is not None
+        # todo ：测试数据要放到数据文件中
+        group_name = "UI"
+        tag_name = [{"name": "web"}, {"name": "app"}, {"name": "H5"}]
+        r = self.tag.add(group_name=group_name, tag_name=tag_name)
+        assert r.status_code == 200
+        assert r.json()["errcode"] == 0
 
     @pytest.mark.parametrize("tag_id, tag_name", [
         ['etXTmQCgAAEes0z0UDZBh_kBwovxScQQ', 'tag1_new_'],
@@ -48,3 +47,20 @@ class TestTag:
 
     def test_tag_list_fail(self):
         pass
+
+    # 如果 40068 ，invalid tagid
+    # 0. 添加 tag
+    # 1. 删除 tag 有问题
+    # 2. 再进行重试（重试次数： n）：手动实现，借助 pytest 钩子（rerun插件）
+    #   a. 添加一个接口
+    #   b. 对新添加的接口再删除
+    #   c. 查询删除是否成功
+    def test_delete_group(self):
+        self.tag.delete_group(["etXTmQCgAAuw7_ljUIZ7yVkCFahzS6vw"])
+
+    def test_delete_tag(self):
+        self.tag.delete_tag(["etXTmQCgAAaIpEBNEoeSjkgd_WM4qLkA"])
+
+    def test_delete_and_detect_group(self):
+        r = self.tag.delete_and_detect_group(["et_6ElDwAAyvuY_HFzh0vHvy-yqYhVHA"])
+        assert r.json()["errcode"] == 0
