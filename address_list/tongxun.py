8d4bf608d29b01c9e5e846fde8a0e1a965f5a461
rainbowzhouj <1004385861@qq.com>
2020-12-14 17:33:24 +0800

    - 异常情况的补充

--- address_list/tongxun.py
+++ address_list/tongxun.py
@@ -6,11 +6,11 @@ from address_list.base_api import BaseApi
 
 class PhoneNOGenerator():
     # 随机生成手机号码
-
     def phoneNORandomGenerator(self):
         prelist = ["130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "147", "150", "151", "152",
                    "153", "155", "156", "157", "158", "159", "186", "187", "188"]
         return random.choice(prelist) + "".join(random.choice("0123456789") for i in range(8))
+
 
 class Tongxun(BaseApi):
     def __init__(self):
