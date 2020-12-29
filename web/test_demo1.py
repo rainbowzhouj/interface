71d1d923aa18a62949c724adc7da3f0637b91304
rainbowzhouj <1004385861@qq.com>
2020-12-30 00:34:19 +0800

    selenium shelve数据的读取与存储
    完成web 企业微信实战（一）的作业

--- web/test_demo1.py
+++ web/test_demo1.py
@@ -41,53 +41,62 @@ class TestTestdemo():
         '''
 
         # cookies =self.driver.get_cookies()
+        # print(cookies)
         cookies = [
-            {'domain': '.qq.com', 'expiry': 1672303903, 'httpOnly': False, 'name': '_ga', 'path': '/', 'secure': False,
-             'value': 'GA1.2.419104819.1609231061'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ltype', 'path': '/', 'secure': False,
+             'value': '1'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.vst', 'path': '/', 'secure': False,
+             'value': 'sTzLZS2Bx8bXIlrmAE_S9sYBhrpEH2vtENtW4MWXTvMZrRPImWLH567n7hhU_PIrPxx4Ycj70f8Ph-OMCn5BvVkYqR4GUyGuXrH3qT7skZmV3YWuoFjH1AWzcz88shKJKXkYFfnlA1TB_m8bgT0xDUR67-OUjci6X9PlNWLzKSWEspuX73ZCJKUtyICEphTkIiTgtxLKsJfk68nePBtwxBssXh_DFgAiGlQX7AwjCY6hSF2sGQgjZqN3gKDLsqOsSQKC4pVv1qH4yVz-vTzkFw'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'Hm_lpvt_9364e629af24cb52acc78b43e8c9f77d',
+             'path': '/', 'secure': True, 'value': '1609231781'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1640767781, 'httpOnly': False,
+             'name': 'Hm_lvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': True, 'value': '1609231215'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1609262405, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/',
+             'secure': True, 'value': '964u6pr'},
             {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.vid', 'path': '/', 'secure': False,
              'value': '1688853131096915'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.corpid', 'path': '/', 'secure': False,
-             'value': '1970325014198621'},
             {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.vid', 'path': '/', 'secure': False,
              'value': '1688853131096915'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.sid', 'path': '/', 'secure': False,
-             'value': '7XGEjnZAEoEJacHBxTki7NLgH87ZZPh_77gRbpr-Cx214pFWz3eUJXNDoOM6eQBA'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.vst', 'path': '/', 'secure': False,
-             'value': 'UjRh3yL1L2Dlga9SK16w98-8h5lGp6ONG2MJwSwFdHisjeK3DdCvRQyX802u0341EKhfcYeRW9yrgLWNuOOJq6UpQoUdZceejLpR0G8yNSiDEVRWZYN_VX2MGYWUiQ-3sbzyY88MjZDNDOfz4JMWQuEUHZOKs-sX8QT4WejgWCASkz2bimNInoqL8er1Ouz17mfxZAVnLPDIDKLILsPKPcJvmw1UZfOY8ItTBjEPXWEwzPcO8gUyzryIrFPQ-qDJd2L-fYayQc6rJuBpwv6ZaA'},
-            {'domain': '.work.weixin.qq.com', 'expiry': 1640766869, 'httpOnly': False, 'name': 'wwrtx.c_gdpr',
-             'path': '/', 'secure': False, 'value': '0'},
-            {'domain': '.qq.com', 'expiry': 2147483648, 'httpOnly': False, 'name': 'ptcz', 'path': '/', 'secure': False,
-             'value': '5b315b281e5ec6717655a08c1a5b26c7edbf913e31e2ecee80028f07664225ed'},
-            {'domain': '.work.weixin.qq.com', 'expiry': 1640767781, 'httpOnly': False,
-             'name': 'Hm_lvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': False, 'value': '1609231215'},
-            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvid', 'path': '/',
-             'secure': False, 'value': '9864661754'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ref', 'path': '/', 'secure': False,
-             'value': 'direct'},
-            {'domain': '.qq.com', 'expiry': 2147483648, 'httpOnly': False, 'name': 'RK', 'path': '/', 'secure': False,
-             'value': 'OVSY4udnGz'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'Hm_lpvt_9364e629af24cb52acc78b43e8c9f77d',
-             'path': '/', 'secure': False, 'value': '1609231781'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.refid', 'path': '/', 'secure': False,
-             'value': '02069422'},
-            {'domain': '.qq.com', 'expiry': 1609318303, 'httpOnly': False, 'name': '_gid', 'path': '/', 'secure': False,
+            {'domain': '.qq.com', 'expiry': 1609341642, 'httpOnly': False, 'name': '_gid', 'path': '/', 'secure': False,
              'value': 'GA1.2.583006214.1609231061'},
             {'domain': 'work.weixin.qq.com', 'expiry': 1609262405, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/',
              'secure': False, 'value': '964u6pr'},
-            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvi', 'path': '/',
-             'secure': False, 'value': '4031836160'},
-            {'domain': '.qq.com', 'expiry': 1911736129, 'httpOnly': False, 'name': 'tvfe_boss_uuid', 'path': '/',
-             'secure': False, 'value': 'f92881bfdb8355d9'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.sid', 'path': '/', 'secure': False,
+             'value': '7XGEjnZAEoEJacHBxTki7FCQ4u9RmUFxVSeFJ2gtpRCY3Ph4v1MsOwho_QCkzJNi'},
             {'domain': '.qq.com', 'expiry': 1911470500, 'httpOnly': False, 'name': 'pac_uid', 'path': '/',
-             'secure': False, 'value': '0_5f22b6a456b8d'},
-            {'domain': '.work.weixin.qq.com', 'expiry': 1611824131, 'httpOnly': False, 'name': 'wwrtx.i18n_lan',
-             'path': '/', 'secure': False, 'value': 'zh'}]
-        # shelve python 内置模块，专门用来对数据进行持久化存储的库，相当于小型的数据库
-        # 可以通过 key，value 的形式把数据保存到shelve中
+             'secure': True, 'value': '0_5f22b6a456b8d'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1640766869, 'httpOnly': False, 'name': 'wwrtx.c_gdpr',
+             'path': '/', 'secure': True, 'value': '0'},
+            {'domain': '.qq.com', 'expiry': 1672327242, 'httpOnly': False, 'name': '_ga', 'path': '/', 'secure': False,
+             'value': 'GA1.2.419104819.1609231061'},
+            {'domain': '.qq.com', 'expiry': 2147483648, 'httpOnly': False, 'name': 'ptcz', 'path': '/', 'secure': True,
+             'value': '5b315b281e5ec6717655a08c1a5b26c7edbf913e31e2ecee80028f07664225ed'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.d2st', 'path': '/', 'secure': False,
+             'value': 'a355959'},
+            {'domain': '.qq.com', 'expiry': 2147483648, 'httpOnly': False, 'name': 'RK', 'path': '/', 'secure': True,
+             'value': 'OVSY4udnGz'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ref', 'path': '/', 'secure': True,
+             'value': 'direct'},
+            {'domain': '.qq.com', 'expiry': 1911736129, 'httpOnly': False, 'name': 'tvfe_boss_uuid', 'path': '/',
+             'secure': True, 'value': 'f92881bfdb8355d9'},
+            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvid', 'path': '/',
+             'secure': True, 'value': '9864661754'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.refid', 'path': '/', 'secure': True,
+             'value': '02069422'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1611849418, 'httpOnly': False, 'name': 'wwrtx.i18n_lan',
+             'path': '/', 'secure': False, 'value': 'zh'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.corpid', 'path': '/', 'secure': False,
+             'value': '1970325014198621'},
+            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvi', 'path': '/',
+             'secure': True, 'value': '4031836160'}]
+        '''
+        shelve python 内置模块，专门用来对数据进行持久化存储的库，相当于小型的数据库
+        可以通过 key，value 的形式把数据保存到shelve中
+        '''
         db = shelve.open('cookies')
         db['cookie'] = cookies
         db.close()
-
+        #
         self.driver.get('https://work.weixin.qq.com/wework_admin/frame')
         for cookie in cookies:
             self.driver.add_cookie(cookie)
@@ -95,4 +104,80 @@ class TestTestdemo():
         # self.driver.get('https://work.weixin.qq.com/wework_admin/frame')
         self.driver.refresh()
         sleep(3)
-        # print(cookies)
+        self.driver.find_element(By.CSS_SELECTOR, ".index_service_cnt_itemWrap:nth-child(2)").click()
+        self.driver.find_element(By.CSS_SELECTOR, '.ww_fileImporter_fileContainer_uploadInputMask').send_keys(
+            'C:\\Users\\10043\\Documents\\WXWork\\1688853721922796\\Cache\\File\\2020-04\\rtod3.xlsx')
+        filename = self.driver.find_element(By.CSS_SELECTOR, '.ww_fileImporter_fileContainer_fileNames').text
+        assert "rtod3.xlsx" == filename
+        sleep(3)
+
+    def test_shelve(self):
+        cookies = [
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ltype', 'path': '/', 'secure': False,
+             'value': '1'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.vst', 'path': '/', 'secure': False,
+             'value': 'sTzLZS2Bx8bXIlrmAE_S9sYBhrpEH2vtENtW4MWXTvMZrRPImWLH567n7hhU_PIrPxx4Ycj70f8Ph-OMCn5BvVkYqR4GUyGuXrH3qT7skZmV3YWuoFjH1AWzcz88shKJKXkYFfnlA1TB_m8bgT0xDUR67-OUjci6X9PlNWLzKSWEspuX73ZCJKUtyICEphTkIiTgtxLKsJfk68nePBtwxBssXh_DFgAiGlQX7AwjCY6hSF2sGQgjZqN3gKDLsqOsSQKC4pVv1qH4yVz-vTzkFw'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'Hm_lpvt_9364e629af24cb52acc78b43e8c9f77d',
+             'path': '/', 'secure': True, 'value': '1609231781'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1640767781, 'httpOnly': False,
+             'name': 'Hm_lvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': True, 'value': '1609231215'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1609262405, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/',
+             'secure': True, 'value': '964u6pr'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.vid', 'path': '/', 'secure': False,
+             'value': '1688853131096915'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.vid', 'path': '/', 'secure': False,
+             'value': '1688853131096915'},
+            {'domain': '.qq.com', 'expiry': 1609341642, 'httpOnly': False, 'name': '_gid', 'path': '/', 'secure': False,
+             'value': 'GA1.2.583006214.1609231061'},
+            {'domain': 'work.weixin.qq.com', 'expiry': 1609262405, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/',
+             'secure': False, 'value': '964u6pr'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.sid', 'path': '/', 'secure': False,
+             'value': '7XGEjnZAEoEJacHBxTki7FCQ4u9RmUFxVSeFJ2gtpRCY3Ph4v1MsOwho_QCkzJNi'},
+            {'domain': '.qq.com', 'expiry': 1911470500, 'httpOnly': False, 'name': 'pac_uid', 'path': '/',
+             'secure': True, 'value': '0_5f22b6a456b8d'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1640766869, 'httpOnly': False, 'name': 'wwrtx.c_gdpr',
+             'path': '/', 'secure': True, 'value': '0'},
+            {'domain': '.qq.com', 'expiry': 1672327242, 'httpOnly': False, 'name': '_ga', 'path': '/', 'secure': False,
+             'value': 'GA1.2.419104819.1609231061'},
+            {'domain': '.qq.com', 'expiry': 2147483648, 'httpOnly': False, 'name': 'ptcz', 'path': '/', 'secure': True,
+             'value': '5b315b281e5ec6717655a08c1a5b26c7edbf913e31e2ecee80028f07664225ed'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.d2st', 'path': '/', 'secure': False,
+             'value': 'a355959'},
+            {'domain': '.qq.com', 'expiry': 2147483648, 'httpOnly': False, 'name': 'RK', 'path': '/', 'secure': True,
+             'value': 'OVSY4udnGz'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ref', 'path': '/', 'secure': True,
+             'value': 'direct'},
+            {'domain': '.qq.com', 'expiry': 1911736129, 'httpOnly': False, 'name': 'tvfe_boss_uuid', 'path': '/',
+             'secure': True, 'value': 'f92881bfdb8355d9'},
+            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvid', 'path': '/',
+             'secure': True, 'value': '9864661754'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.refid', 'path': '/', 'secure': True,
+             'value': '02069422'},
+            {'domain': '.work.weixin.qq.com', 'expiry': 1611849418, 'httpOnly': False, 'name': 'wwrtx.i18n_lan',
+             'path': '/', 'secure': False, 'value': 'zh'},
+            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.corpid', 'path': '/', 'secure': False,
+             'value': '1970325014198621'},
+            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvi', 'path': '/',
+             'secure': True, 'value': '4031836160'}]
+        '''
+        shelve python 内置模块，专门用来对数据进行持久化存储的库，相当于小型的数据库
+        可以通过 key，value 的形式把数据保存到shelve中
+        '''
+        db = shelve.open('cookies')
+        # 存储后如何更新？？
+        db['cookie'] = cookies
+        db.close()
+        # db = shelve.open('cookies')
+        # cookies=db['cookie']
+        # db.close()
+        self.driver.get('"https://work.weixin.qq.com/wework_admin/frame')
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
+
+        self.driver.refresh()
+        # self.driver.find_element(By.CSS_SELECTOR,'.index_service_cnt_item_title:nth-child(2)').click()
+        # self.driver.find_element(By.CSS_SELECTOR, ".index_service_cnt_itemWrap:nth-child(2)").click()
+        # self.driver.find_element(By.CSS_SELECTOR,'.ww_fileImporter_fileContainer_uploadInputMask').send_keys('C:\\Users\\10043\\Documents\\WXWork\\1688853721922796\\Cache\\File\\2020-04\\rtod3.xlsx')
+        # filename=self.driver.find_element(By.CSS_SELECTOR,'.ww_fileImporter_fileContainer_fileNames').text
+        # assert "rtod3.xlsx"==filename
+        #sleep(3)
